c44b57aa6d7c9c206132550a1a616e60b1bb0cbf
Deydre <deydrealonso@gmail.com>
2024-11-05 09:14:54 +0100

    fixed/routes

--- routes/providers.routes.js
+++ routes/providers.routes.js
@@ -3,9 +3,6 @@ const router = require('express').Router();
 
 // [GET] TODOS http://localhost:3000/api/providers 
 router.get("/", providersController.getProvider);
-// [GET] UNO http://localhost:3000/api/providers + body {companyName}
-// router.get("/getOne/", providersController.getOneProvider);
-
 // POST http://localhost:3000/api/providers + body con {companyName, website, image}
 router.post("/", providersController.createProvider);
 router.put("/:id", providersController.editProvider);
